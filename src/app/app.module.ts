5570cf4a00162538e891ce1a22a41e7a93da3c23
Emilio Roman Alegria <eranet1973@hotmail.com>
2021-08-05 08:41:43 -0400

    Primer Commit carpetas inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './components/inicio/inicio.component';
+import { BienvenidaComponent } from './components/inicio/bienvenida/bienvenida.component';
+import { LoginComponent } from './components/inicio/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    BienvenidaComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  endpoint : 'https://200.14.245.112:50015/b1s/v1/'
 };
 
 /*
